8ac3916e95b2a01099a31fe3313eab32defdd6b7
Arailym <arailym.bakenovaa@gmail.com>
2023-02-25 16:42:28 +0600

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Арайлым Бакенова on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

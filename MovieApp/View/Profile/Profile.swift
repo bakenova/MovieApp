09f512c2687c0971229dd7f5e3f299ccbeecc70e
Arailym <arailym.bakenovaa@gmail.com>
2023-04-27 05:09:31 +0600

    Add user info view, buttonView

--- MovieApp/View/Profile/Profile.swift
+++ MovieApp/View/Profile/Profile.swift
@@ -11,56 +11,72 @@ struct Profile: View {
     @State var currentTab: String = "Comics"
     @Namespace var animation
     @Environment(\.colorScheme) var scheme
+    
+    var profileSettings: [String] = ["My Account","Subscription Info", "Payment Info", "Dark mode", "Delete Account"]
     var body: some View {
-        VStack{
+        NavigationView{
             VStack{
-                ZStack{
-                    Circle()
-                        .fill(.purple)
-                        .frame(width: 145, height: 145)
-                    Circle()
-                        .fill(self.scheme == .dark ? .black : .white)
-                        .frame(width: 140, height: 140)
-                    Image("user")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 128, height: 128)
-                }
-                .padding()
-                
-                Text("Arailym")
-                    .font(.title2)
-                    .fontWeight(.black)
-                    .foregroundColor(.primary)
-                    .padding(.bottom, 5)
-                
-                Text(verbatim: "arailym.bakenovaa@gmail.com")
-                    .font(.body)
-                    .foregroundColor(.gray)
-                
-            }.padding()
-            
-            ZStack{
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(self.scheme == .dark ? .gray : .yellow.opacity(0.2))
-                    .opacity(self.scheme == .dark ? 0.5 : 1)
-                    .ignoresSafeArea()
-                VStack(alignment: .leading){
-                    Text("User Info")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                        .padding(.top, 30)
-                        .padding(.horizontal, 30)
-                    List{
-                        ForEach((1...5), id: \.self) {
-                                        Text("Row \($0)")
-                                .listRowBackground(Color.gray.opacity(0))
-                                    }
+                VStack{
+                    ZStack{
+                        Circle()
+                            .fill(.purple)
+                            .frame(width: 145, height: 145)
+                        Circle()
+                            .fill(self.scheme == .dark ? .black : .white)
+                            .frame(width: 140, height: 140)
+                        Image("user")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 128, height: 128)
                     }
-                    .background(.gray.opacity(0))
-                    .scrollContentBackground(.hidden)
-                    //.padding(.horizontal, 30)
-                    .padding(.bottom, 30)
+                    .padding()
+                    
+                    Text("Arailym")
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .padding(.bottom, 5)
+                    
+                    Text(verbatim: "arailym.bakenovaa@gmail.com")
+                        .font(.body)
+                        .foregroundColor(.gray)
+                    
+                }.padding()
+                
+                ZStack(){
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(self.scheme == .dark ? .gray : .yellow.opacity(0.2))
+                        .opacity(self.scheme == .dark ? 0.5 : 1)
+                        .ignoresSafeArea()
+                    
+                    VStack(alignment: .leading){
+                        Text("User Info")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                            .padding(.top, 30)
+                            .padding(.bottom, 5)
+                            .padding(.horizontal, 30)
+                        
+                        List{
+                            ForEach(profileSettings, id: \.self){ setting in
+                                Text(setting.description)
+                                    .padding(.vertical, 5)
+                                    .font(.system(size: 24, weight: .medium))
+                                    .foregroundColor(.primary)
+                                    .listRowBackground(Color.gray.opacity(0))
+                            }
+                        }
+                        .listStyle(PlainListStyle())
+                        .background(.gray.opacity(0))
+                        .scrollContentBackground(.hidden)
+                        .scrollDisabled(true)
+                        .padding(.vertical, 0)
+                        .padding(.horizontal, 16)
+                        
+                        ButtonView(title: "Sign Out", color: Color(.systemOrange))
+                            .padding(.leading, 80)
+                            .padding(.vertical, 30)
+                    }
                 }
             }
         }
@@ -70,6 +86,22 @@ struct Profile: View {
 struct Comics_Previews: PreviewProvider {
     static var previews: some View {
         Profile()
-            .preferredColorScheme(.light)
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct ButtonView: View {
+    
+    var title: String
+    var color: Color
+    
+    var body: some View {
+        Text(title)
+            .bold()
+            .font(.title2)
+            .frame(width: 280, height: 50)
+            .background(color)
+            .foregroundColor(.white)
+            .cornerRadius(15)
     }
 }
